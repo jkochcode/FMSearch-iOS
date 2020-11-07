9147eba1e509f6b27862ea3560253dcd9ce90c84
Josh Koch <koch.josh25@gmail.com>
2020-11-06 22:47:10 -0800

    Initial Commit

---
+++ FMSearch/FMSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FMSearchApp.swift
+//  FMSearch
+//
+//  Created by Josh Koch on 11/6/20.
+//
+
+import SwiftUI
+
+@main
+struct FMSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
